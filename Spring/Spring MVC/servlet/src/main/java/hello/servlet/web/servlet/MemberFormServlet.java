6d5d2f769c64c8e78bf0ef6ace5112e634cfe6f9
Jeon <jeon9771@naver.com>
2022-08-14 17:04:07 +0900

    Servlet, JSP, MVC Pattern

--- Spring/Spring MVC/servlet/src/main/java/hello/servlet/web/servlet/MemberFormServlet.java
+++ Spring/Spring MVC/servlet/src/main/java/hello/servlet/web/servlet/MemberFormServlet.java
@@ -17,21 +17,22 @@ public class MemberFormServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/plain");
+
+        response.setContentType("text/html");
         response.setCharacterEncoding("utf-8");
 
-        PrintWriter writer = response.getWriter();
-        writer.write("<!DOCTYPE html>\n" +
+        PrintWriter w = response.getWriter();
+        w.write("<!DOCTYPE html>\n" +
                 "<html>\n" +
                 "<head>\n" +
-                " <meta charset=\"UTF-8\">\n" +
-                " <title>Title</title>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <title>Title</title>\n" +
                 "</head>\n" +
                 "<body>\n" +
                 "<form action=\"/servlet/members/save\" method=\"post\">\n" +
-                " username: <input type=\"text\" name=\"username\" />\n" +
-                " age: <input type=\"text\" name=\"age\" />\n" +
-                " <button type=\"submit\">전송</button>\n" +
+                "    username: <input type=\"text\" name=\"username\" />\n" +
+                "    age:      <input type=\"text\" name=\"age\" />\n" +
+                "    <button type=\"submit\">전송</button>\n" +
                 "</form>\n" +
                 "</body>\n" +
                 "</html>\n");
