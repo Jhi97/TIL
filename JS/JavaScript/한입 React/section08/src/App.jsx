a55f57456bb56f0ce6b9027ae2b589d1c2d949a8
jhi97 <jeon9771@naver.com>
2025-05-08 23:40:01 +0900

    리액트 4일차

--- JS/JavaScript/한입 React/section08/src/App.jsx
+++ JS/JavaScript/한입 React/section08/src/App.jsx
@@ -2,13 +2,70 @@ import './App.css';
 import Header from './components/Header';
 import Editor from './components/Editor';
 import List from './components/List';
+import { useState, useRef } from 'react';
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: 'React 공부하기',
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: '출근하기',
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: '데이트하기',
+    date: new Date().getTime(),
+  },
+];
 
 function App() {
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+
+    //todos.push(newTodo) 로 추가 금지 -> 상태변화 감지를 위함
+    setTodos([newTodo, ...todos]);
+  };
+
+  const onUpdate = (targetId) => {
+    //todos State의 값들 중에
+    //targetId와 일치한 id를 갖는 투두 아이템의 isDone 변경
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === targetId) {
+          return {
+            ...todo,
+            isDone: !todo.isDone,
+          };
+        }
+        return todo;
+      })
+    );
+  };
+
+  const onDelete = (targetId) => {
+    setTodos(todos.filter((todo) => todo.id !== targetId));
+  };
+
   return (
     <div className="App">
       <Header />
-      <Editor />
-      <List />
+      <Editor onCreate={onCreate} />
+      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
     </div>
   );
 }
--- JS/JavaScript/한입 React/section08/src/components/List.jsx
+++ JS/JavaScript/한입 React/section08/src/components/List.jsx
@@ -1,16 +1,45 @@
 import './List.css';
 import TodoItem from './TodoItem';
+import { useState } from 'react';
 
-export default function List() {
+export default function List({ todos, onUpdate, onDelete }) {
+  const [search, setSearch] = useState('');
+
+  const onChagneSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === '') {
+      return todos;
+    }
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLocaleLowerCase())
+    );
+  };
+
+  const filteredTodos = getFilteredData();
+
   return (
     <div className="List">
       <h4>Todo List 🌱</h4>
-      <input placeholder="검색어를 입력하세요" />
+      <input
+        onChange={onChagneSearch}
+        value={search}
+        placeholder="검색어를 입력하세요"
+      />
 
       <div className="todos_wrapper">
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {filteredTodos.map((todo) => {
+          return (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              onUpdate={onUpdate}
+              onDelete={onDelete}
+            />
+          );
+        })}
       </div>
     </div>
   );
