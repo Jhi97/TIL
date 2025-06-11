9bd9307df032887486d50fd7adefc08bbb37d1c0
jhi97 <jeon9771@naver.com>
2025-06-11 23:12:38 +0900

    Spring Boot + React 연동 연습

---
+++ Toy/testReact/src/test/java/org/example/testreact/TestReactApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.testreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
