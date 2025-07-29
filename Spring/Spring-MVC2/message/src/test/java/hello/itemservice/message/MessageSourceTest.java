1ac0182e0e4074801a38277aa58b86377a579a9b
jhi97 <jeon9771@naver.com>
2025-07-29 23:05:44 +0900

    Sprint MVC2 REMIND Day1

--- Spring/Spring-MVC2/message/src/test/java/hello/itemservice/message/MessageSourceTest.java
+++ Spring/Spring-MVC2/message/src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -1,5 +1,6 @@
 package hello.itemservice.message;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,8 +8,6 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.NoSuchMessageException;
 
 import java.util.Locale;
-
-import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 public class MessageSourceTest {
@@ -18,36 +17,32 @@ public class MessageSourceTest {
 
     @Test
     void helloMessage() {
-        String result = ms.getMessage("hello", null, null);
-        assertThat(result).isEqualTo("안녕");
+        String hello = ms.getMessage("hello", null, null);
+        System.out.println("hello = " + hello);
+        Assertions.assertThat(hello).isEqualTo("안녕");
     }
 
     @Test
     void notFoundMessageCode() {
-        assertThatThrownBy(() -> ms.getMessage("no_code", null, null))
-                .isInstanceOf(NoSuchMessageException.class);
+        Assertions.assertThatThrownBy(() -> ms.getMessage("no_code", null, null)).isInstanceOf(NoSuchMessageException.class);
     }
 
     @Test
-    void notFoundMessageCodeDefaultMessage() {
-        String result = ms.getMessage("no_code", null, "기본 메시지", null);
-        assertThat(result).isEqualTo("기본 메시지");
+    void defaultMessageCode() {
+        String message = ms.getMessage("no_code", null, "기본 메시지", null);
+        Assertions.assertThat(message).isEqualTo("기본 메시지");
     }
 
     @Test
     void argumentMessage() {
-        String result = ms.getMessage("hello.name", new Object[]{"Spring"}, null);
-        assertThat(result).isEqualTo("안녕 Spring");
+        String message = ms.getMessage("hello.name", new Object[]{"Spring"}, null);
+        Assertions.assertThat(message).isEqualTo("안녕 Spring");
     }
 
     @Test
     void defaultLang() {
-        assertThat(ms.getMessage("hello", null, null)).isEqualTo("안녕");
-        assertThat(ms.getMessage("hello", null, null, Locale.KOREA)).isEqualTo("안녕");
-    }
-
-    @Test
-    void enLang() {
-        assertThat(ms.getMessage("hello", null, Locale.ENGLISH)).isEqualTo("hello");
+        Assertions.assertThat(ms.getMessage("hello", null, Locale.ENGLISH)).isEqualTo("hello");
+        Assertions.assertThat(ms.getMessage("hello", null, null)).isEqualTo("안녕");
+        Assertions.assertThat(ms.getMessage("hello", null, Locale.KOREA)).isEqualTo("안녕");
     }
 }
