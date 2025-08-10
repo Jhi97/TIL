ee1ad9a362e0bd1ab0ea6d398b27b9b6b7b76c06
jhi97 <jeon9771@naver.com>
2025-08-10 22:28:50 +0900

    Spring MVC2 REMIND exception day 2

--- Spring/Spring-MVC2/exception/src/main/java/hello/exception/WebServerCustomizer.java
+++ Spring/Spring-MVC2/exception/src/main/java/hello/exception/WebServerCustomizer.java
@@ -3,10 +3,10 @@ package hello.exception;
 import org.springframework.boot.web.server.ConfigurableWebServerFactory;
 import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 
-@Configuration
+//@Component
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
---
+++ Spring/Spring-MVC2/exception/src/main/java/hello/exception/resolver/MyHandlerException.java
@@ -0,0 +1,26 @@
+package hello.exception.resolver;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+public class MyHandlerException implements HandlerExceptionResolver {
+
+
+    @Override
+    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        try {
+            if (ex instanceof IllegalArgumentException) {
+                log.info("IllegalArgumentException resolver to 400");
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
+                return new ModelAndView(); //예외 처리 후 정상 흐름으로 변경하는 것이 목적
+            }
+        } catch (Exception e) {
+            log.error("resolver ex", e);
+        }
+        return null;
+    }
+}
