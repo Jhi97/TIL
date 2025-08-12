c490032ad70e4011ddcb064361ba4507d8b37e00
jhi97 <jeon9771@naver.com>
2025-08-12 23:12:17 +0900

    Spring MVC2 REMIND Exception day4

--- Spring/Spring-MVC2/exception/src/main/java/hello/exception/api/ApiExceptionV2Controller.java
+++ Spring/Spring-MVC2/exception/src/main/java/hello/exception/api/ApiExceptionV2Controller.java
@@ -1,20 +1,26 @@
 package hello.exception.api;
 
-import hello.exception.exception.BadRequestException;
 import hello.exception.exception.UserException;
+import hello.exception.exhandler.ErrorResult;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.http.HttpStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
+@Slf4j
 @RestController
-public class ApiExceptionController {
+public class ApiExceptionV2Controller {
 
-    @GetMapping("/api/members/{id}")
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ErrorResult illegalExHandler(IllegalArgumentException e) {
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("BAD", e.getMessage());
+    }
+
+    @GetMapping("/api2/members/{id}")
     public MemberDto getMember(@PathVariable("id") String id) {
         System.out.println("ApiExceptionController.getMember");
         if (id.equals("ex")) {
@@ -32,21 +38,6 @@ public class ApiExceptionController {
         return new MemberDto(id, "hello " + id);
     }
 
-    @GetMapping("/api/response-status-ex1")
-    public String responseStatusEx1() {
-        throw new BadRequestException();
-    }
-
-    @GetMapping("/api/response-status-ex2")
-    public String responseStatusEx2() {
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad",
-                new IllegalArgumentException());
-    }
-
-    @GetMapping("/api/default-handler-ex")
-    public String defaultException(@RequestParam Integer data) {
-        return "ok";
-    }
 
     @Data
     @AllArgsConstructor
