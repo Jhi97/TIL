88cab17427a8fe29e0cc58699fc02ff5e251a3f1
jhi97 <jeon9771@naver.com>
2025-08-17 15:51:27 +0900

    Spring MVC2 REMIND Converter, Formatter

---
+++ Spring/Spring-MVC2/typeconverter/src/main/java/hello/typeconverter/type/IpPort.java
@@ -0,0 +1,17 @@
+package hello.typeconverter.type;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter
+@EqualsAndHashCode //모든 필드를 사용해 equals(), hashCode() 생성
+public class IpPort {
+
+    private String ip;
+    private int port;
+
+    public IpPort(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+    }
+}
